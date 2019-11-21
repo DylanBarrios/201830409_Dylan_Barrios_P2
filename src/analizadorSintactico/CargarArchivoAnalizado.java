1b2c056ebd56a237bd628f8ea5ebd82ddea4462b
dylan <dylanbarrios201830409@cunoc.edu.gt>
2019-11-21 02:45:45 -0600

    analizis sintactico

---
+++ src/analizadorSintactico/CargarArchivoAnalizado.java
@@ -0,0 +1,52 @@
+package analizadorSintactico;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import javax.swing.JFileChooser;
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author dylan
+ */
+public class CargarArchivoAnalizado {
+    AnalizadorSintactico anal;
+    JFileChooser select = new JFileChooser();
+    File files;
+    FileInputStream in;
+    FileOutputStream out;
+    public String docCargado;
+    
+    
+    public String cargar(File files) {
+        String doc = "";
+        try {
+            in = new FileInputStream(files);
+            int ascci;
+            while ((ascci = in.read()) != -1) {
+                char caracter = (char) ascci;
+                doc += caracter;
+            }
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+        return doc;
+    }
+
+    public void verArchivo() {
+        if (select.showDialog(null, "Abrir") == JFileChooser.APPROVE_OPTION) {
+            files = select.getSelectedFile();
+            if (files.canRead()) {
+                if (files.getName().endsWith("txt")) {
+                    docCargado = cargar(files);
+                    anal = new AnalizadorSintactico();
+                    anal.analizis(docCargado);
+                } else {
+                    JOptionPane.showMessageDialog(null, "Archivo No Compatible");
+                }
+            }
+        }
+    }
+}
